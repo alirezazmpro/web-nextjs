9596eaba1eff0c7642663d5f25710e856e8c98a7
Alireza <zamani802000@gmail.com>
2023-02-19 17:51:06 +0330

    Create project

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,22 @@
-import './globals.css'
-
+import "./globals.css";
+import Providers from "./Provider";
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
-      <head />
-      <body>{children}</body>
-    </html>
-  )
+    <Providers>
+      <html lang="en">
+        <head>
+          <link
+            href="https://cdn.jsdelivr.net/gh/rastikerdar/vazirmatn@v33.003/Vazirmatn-font-face.css"
+            rel="stylesheet"
+            type="text/css"
+          />
+        </head>
+        <body>{children}</body>
+      </html>
+    </Providers>
+  );
 }
